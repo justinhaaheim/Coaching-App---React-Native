797a90d9508015f3ab4e7c548a02fcacc3901968
Justin H <io@justinh.org>
2017-03-31 13:13:41 -0400

    Added different version of arena questions and Empower launch screen

--- App.js
+++ App.js
@@ -83,7 +83,7 @@ export default class App extends React.Component {
 
     render() {
 
-        let qualities = [
+        const qualities = [
             'Alert',
             'Attentive',
             'Appreciative',
@@ -109,12 +109,22 @@ export default class App extends React.Component {
             'Vulnerable'
         ];
 
-        let questionsMLE = {
+        const questionsMLE = {
             1: "Who am I willing to be in order to produce an extraordinary result out of this interaction?",
             2: "Am I willing to systematically dismantle my structure of knowing?",
             3: "Am I willing to be a demand for coaching?",
             4: "Am I willing to guarantee that whoever coaches me will be successful?"
         };
+
+        const questionsClient = {
+            1: "There are some qualities I am willing to demonstrate in this interaction so we can both make sure we have an extraordinary result out of our session together:",
+            2: "During the session am I willing to set aside the opinions, beliefs, and thoughts that may be getting in the way of seeing something clearly?",
+            3: "Am I willing to be open minded and fully engaged in order to receive maximum benefit from this time together?",
+            4: "The result of coaching in this model is always action. Am I willing to see the most authentic action to take? If I am the person being coached, am I willing to promise to take it?"
+        }
+
+        // defaulting to Client questions. Todo: build a setting where the user can switch this.
+        const questions = questionsClient;
 
         let buttons = qualities.map((qname, index) => (
         <View key={`button-view${index}`}
@@ -210,11 +220,11 @@ export default class App extends React.Component {
                         marginVertical: 10,
                     }}>
                         <Text style={styles.h2}>ARENA QUESTIONS</Text>
-                        <Text style={styles.textQuestion}>1) {questionsMLE[1]}</Text>
+                        <Text style={styles.textQuestion}>1) {questions[1]}</Text>
                         <Text style={[styles.qField, styles.textQuestion]}>{`I am willing to be ${this.qualitiesList()}.`}</Text>
-                        <Text style={styles.textQuestion}>2) {questionsMLE[2]}</Text>
-                        <Text style={styles.textQuestion}>3) {questionsMLE[3]}</Text>
-                        <Text style={styles.textQuestion}>4) {questionsMLE[4]}</Text>
+                        <Text style={styles.textQuestion}>2) {questions[2]}</Text>
+                        <Text style={styles.textQuestion}>3) {questions[3]}</Text>
+                        <Text style={styles.textQuestion}>4) {questions[4]}</Text>
                         <Text style={styles.copyright}>{`The Coaching Arena © 1997-${(new Date()).getFullYear()} The Academy for Coaching Excellence`}</Text>
                     </View>
 
