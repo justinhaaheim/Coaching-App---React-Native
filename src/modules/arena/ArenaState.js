f886f9624ec8ddd1e796123e8d2f2da2cca13415
Justin H <io@justinh.org>
2017-07-13 11:02:21 -0700

    clarified/refactored

--- src/modules/arena/ArenaState.js
+++ src/modules/arena/ArenaState.js
@@ -66,42 +66,21 @@ export default function ArenaStateReducer(state = initialState, action = {}) {
       return initialState;
     }
     case TOGGLE_BUTTON: {
-      // console.log('TOGGLE_BUTTON action is being taken.');
-      // console.log(`action.type=${action.type} and action.payload=${action.payload}`);
-      // payload is an index into the button array
-      // Does this need to be .getIn(['arena', 'buttons'])?
-      // const { buttons } = state;
-      // console.log("action: ", action);
-      // console.log(typeof action.payload);
-      // console.log(state.buttons[action.payload].selected);
-
-      const newButtons = state.buttons.map((bttn, index) =>
-        index === action.payload ?
-        Object.assign({}, bttn, {selected: !bttn.selected}) :
-        bttn
+      const newButtons = state.buttons.map(
+        (bttn, index) =>
+          index === action.payload ? Object.assign({}, bttn, { selected: !bttn.selected }) : bttn,
       );
 
       // If the change would result in >= 5 buttons, return state unmodified
       if (!state.buttons[action.payload].selected) {
-        if (state.buttons.filter( b => b.selected ).length >= 5) {
+        if (state.buttons.filter(b => b.selected).length >= 5) {
           return state;
         }
       }
 
       return Object.assign({}, state, {
-        buttons: newButtons
+        buttons: newButtons,
       });
-
-      // Can i use the {...} spreader syntax?
-//       return Object.assign({}, state, {
-//         buttons: state.buttons.map(
-//           (b, index) => {
-// //            index === action.payload ?
-//             Object.assign({}, b, { selected: 44 }) // :
-// //            b
-//           }
-//       )});
-
     }
     default: {
       return state;
@@ -109,8 +88,5 @@ export default function ArenaStateReducer(state = initialState, action = {}) {
   }
 }
 
-
 // Selectors
-export const getQualitiesList = (state) => {
-  return state.buttons.filter(b => b.selected).map(b => b.name);
-}
+export const getQualitiesList = state => state.buttons.filter(b => b.selected).map(b => b.name);
