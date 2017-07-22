f6b1d4fc0f2fc61dc32e693162652f93694315f5
Justin H <io@justinh.org>
2017-07-22 14:13:41 -0400

    mobilecenter integreated - staging not working

---
+++ ios/Pods/Target Support Files/Pods-EmpowerApp/Pods-EmpowerApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EmpowerApp : NSObject
+@end
+@implementation PodsDummy_Pods_EmpowerApp
+@end
---
+++ ios/Pods/Target Support Files/Pods-EmpowerAppTests/Pods-EmpowerAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EmpowerAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_EmpowerAppTests
+@end
